f059ebd4de12b0fe9bbbad6fefac60d2ad1287cd
w11wo <wilsonwong961@gmail.com>
2024-08-08 20:51:40 +1000

    Updated SBERT to v3; Added New Datasets

--- training/all/all_datasets.py
+++ training/all/all_datasets.py
@@ -1,10 +1,11 @@
-from typing import List
 from dataclasses import dataclass
 import random
+import os
 
-from datasets import load_dataset
-from sentence_transformers import InputExample
+from datasets import load_dataset, concatenate_datasets, Dataset
 import numpy as np
+
+kwargs = {"trust_remote_code": True, "num_proc": min(4, os.cpu_count() - 4)}
 
 ##############
 # PAIRS
@@ -12,139 +13,119 @@ import numpy as np
 
 
 @dataclass
-class MultilingualNLI:
-    dataset = load_dataset("LazarusNLP/multilingual-NLI-26lang-2mil7-id", split="train", trust_remote_code=True)
+class MultilingualNLIEntailmentPairs:
+    dataset = load_dataset("LazarusNLP/multilingual-NLI-26lang-2mil7-id", split="train", **kwargs)
     # filter for entailment pairs
     dataset = dataset.filter(lambda example: example["label"] == 0)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in MultilingualNLI.dataset:
-            train_samples.append(InputExample(texts=[datum["premise"], datum["hypothesis"]]))
-
-        return train_samples
+    dataset = dataset.select_columns(["premise", "hypothesis"])
 
 
 @dataclass
-class WReTE:
-    dataset = load_dataset("SEACrowd/wrete", split="train", trust_remote_code=True)
+class WReTEEntailmentPairs:
+    dataset = load_dataset("SEACrowd/wrete", split="train", **kwargs)
     # filter for entailment pairs
     dataset = dataset.filter(lambda example: example["label"] == "Entail_or_Paraphrase")
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in WReTE.dataset:
-            train_samples.append(InputExample(texts=[datum["sent_A"], datum["sent_B"]]))
-
-        return train_samples
+    dataset = dataset.select_columns(["sent_A", "sent_B"])
 
 
 @dataclass
-class IndoLEMNTP:
-    dataset = load_dataset("SEACrowd/indolem_ntp", split="train", trust_remote_code=True)
+class IndoLEMNTPEntailmentPairs:
+    dataset = load_dataset("SEACrowd/indolem_ntp", split="train", **kwargs)
     # filter for entailment pairs
     dataset = dataset.filter(lambda example: example["label"] == 1)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in IndoLEMNTP.dataset:
-            train_samples.append(InputExample(texts=[datum["tweets"], datum["next_tweet"]]))
-
-        return train_samples
+    dataset = dataset.select_columns(["tweets", "next_tweet"])
 
 
 @dataclass
 class TyDiQA:
-    dataset = load_dataset("khalidalt/tydiqa-goldp", "indonesian", split="train", trust_remote_code=True).shuffle(
-        seed=42
+    dataset = load_dataset("khalidalt/tydiqa-goldp", "indonesian", split="train", **kwargs)
+    dataset_1 = dataset.map(
+        lambda question, passage: {"query": question, "positive": passage},
+        input_columns=["question_text", "passage_text"],
     )
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in TyDiQA.dataset:
-            train_samples.append(InputExample(texts=[datum["question_text"], datum["passage_text"]]))
-            train_samples.append(InputExample(texts=[datum["question_text"], datum["answers"]["text"][0]]))
-
-        return train_samples
+    dataset_2 = dataset.map(
+        lambda question, answers: {"query": question, "positive": answers["text"][0]},
+        input_columns=["question_text", "answers"],
+    )
+    dataset = concatenate_datasets([dataset_1, dataset_2]).select_columns(["query", "positive"])
 
 
 @dataclass
 class FacQA:
-    dataset = load_dataset("SEACrowd/facqa", split="train", trust_remote_code=True)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in FacQA.dataset:
-            question = " ".join(datum["question"])
-            passage = " ".join(datum["passage"])
-            answer = " ".join(t for t, l in zip(datum["passage"], datum["seq_label"]) if l != "O")
-
-            train_samples.append(InputExample(texts=[question, passage]))
-            train_samples.append(InputExample(texts=[question, answer]))
-
-        return train_samples
+    dataset = load_dataset("SEACrowd/facqa", split="train", **kwargs)
+    dataset_1 = dataset.map(
+        lambda question, passage: {"query": " ".join(question), "positive": " ".join(passage)},
+        input_columns=["question", "passage"],
+    )
+    dataset_2 = dataset.map(
+        lambda question, passage, seq_label: {
+            "query": " ".join(question),
+            "positive": " ".join(t for t, l in zip(passage, seq_label) if l != "O"),
+        },
+        input_columns=["question", "passage", "seq_label"],
+    )
+    dataset = concatenate_datasets([dataset_1, dataset_2]).select_columns(["query", "positive"])
 
 
 @dataclass
 class LFQAID:
-    dataset = load_dataset("indonesian-nlp/lfqa_id", split="train", trust_remote_code=True)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in LFQAID.dataset:
-            question = datum["title"]
-            scores = datum["answers"]["score"]
-            answer = datum["answers"]["text"][np.argmax(scores)]
-
-            train_samples.append(InputExample(texts=[question, answer]))
-
-        return train_samples
+    dataset = load_dataset("indonesian-nlp/lfqa_id", split="train", **kwargs)
+    dataset = dataset.map(
+        lambda title, answers: {"question": title, "answers": answers["text"][np.argmax(answers["score"])]},
+        input_columns=["title", "answers"],
+    )
+    dataset = dataset.select_columns(["question", "answers"])
 
 
 @dataclass
 class IndoQA:
-    dataset = load_dataset("jakartaresearch/indoqa", split="train", trust_remote_code=True)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in IndoQA.dataset:
-            question = datum["question"]
-            passage = datum["context"]
-            answer = datum["answer"]
-
-            if question and passage and answer:
-                train_samples.append(InputExample(texts=[question, passage]))
-                train_samples.append(InputExample(texts=[question, answer]))
-
-        return train_samples
+    dataset = load_dataset("jakartaresearch/indoqa", split="train", **kwargs)
+    dataset_1 = dataset.map(
+        lambda question, context: {"query": question, "positive": context},
+        input_columns=["question", "context"],
+    )
+    dataset_2 = dataset.map(
+        lambda question, answer: {"query": question, "positive": answer},
+        input_columns=["question", "answer"],
+    )
+    dataset = concatenate_datasets([dataset_1, dataset_2]).select_columns(["query", "positive"])
 
 
 @dataclass
 class ParaphraseDetection:
-    dataset = load_dataset("jakartaresearch/id-paraphrase-detection", split="train", trust_remote_code=True)
+    dataset = load_dataset("jakartaresearch/id-paraphrase-detection", split="train", **kwargs)
+    dataset = dataset.select_columns(["sentence1", "sentence2"])
 
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
 
-        for datum in ParaphraseDetection.dataset:
-            train_samples.append(InputExample(texts=[datum["sentence1"], datum["sentence2"]]))
+@dataclass
+class Wikipedia:
+    dataset = load_dataset("wikimedia/wikipedia", "20231101.id", split="train", **kwargs)
+    dataset = dataset.select_columns(["text", "title"])
 
-        return train_samples
+
+@dataclass
+class Brainly:
+    dataset = load_dataset("lesserfield/brainly", split="train", **kwargs)
+    dataset = dataset.filter(lambda status: status == "verified", input_columns=["status_1"])
+    dataset = dataset.select_columns(["instruction", "answer_1"])
+
+
+@dataclass
+class IndonesianNews:
+    dataset = load_dataset("esteler-ai/idn-news-az", split="train", **kwargs)
+    dataset = dataset.select_columns(["text", "title"])
+
+
+@dataclass
+class DoctorQA:
+    dataset = load_dataset("hermanshid/doctor-id-qa", split="train", **kwargs)
+    dataset = dataset.select_columns(["question", "answer"])
+
+
+@dataclass
+class Liputan6:
+    dataset = load_dataset("SEACrowd/liputan6", split="train", **kwargs)
+    dataset = dataset.select_columns(["document", "summary"])
 
 
 ##############
@@ -154,58 +135,48 @@ class ParaphraseDetection:
 
 @dataclass
 class mMARCO:
-    dataset = load_dataset("unicamp-dl/mmarco", "indonesian", split="train", trust_remote_code=True)
-    # limit to only 100,000 rows
-    dataset = dataset.shuffle(seed=42).select(range(100_000))
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in mMARCO.dataset:
-            train_samples.append(
-                InputExample(
-                    texts=[
-                        datum["query"],
-                        datum["positive"],
-                        datum["negative"],
-                    ]
-                )
-            )
-
-        return train_samples
+    dataset = load_dataset("unicamp-dl/mmarco", "indonesian", split="train", **kwargs)
+    dataset = dataset.shuffle(seed=42).select(range(100_000))  # limit to only 100,000 rows
+    dataset = dataset.select_columns(["query", "positive", "negative"])
 
 
 @dataclass
 class MIRACL:
-    dataset = load_dataset("miracl/miracl", "id", split="train", trust_remote_code=True)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in MIRACL.dataset:
-            query = datum["query"]
-            positives = [doc["text"] for doc in datum["positive_passages"]]
-            negatives = [doc["text"] for doc in datum["negative_passages"]]
-
-            if len(negatives) > 0:
-                train_samples.append(InputExample(texts=[query, random.choice(positives), random.choice(negatives)]))
-                train_samples.append(InputExample(texts=[random.choice(positives), query, random.choice(negatives)]))
-
-        return train_samples
+    dataset = load_dataset("miracl/miracl", "id", split="train", **kwargs)
+    dataset = dataset.filter(lambda negatives: len(negatives) > 0, input_columns=["negative_passages"])
+    dataset = dataset.map(
+        lambda query, positives, negatives: {
+            "query": query,
+            "positives": [p["text"] for p in random.sample(positives, min(2, len(positives)))],
+            "negatives": [n["text"] for n in random.sample(negatives, min(2, len(negatives)))],
+        },
+        input_columns=["query", "positive_passages", "negative_passages"],
+    )
+    dataset_1 = dataset.map(
+        lambda query, positives, negatives: {"query": query, "positive": positives[0], "negative": negatives[0]},
+        input_columns=["query", "positives", "negatives"],
+    )
+    dataset_2 = dataset.map(
+        lambda query, positives, negatives: {"query": query, "positive": positives[-1], "negative": negatives[-1]},
+        input_columns=["query", "positives", "negatives"],
+    )
+    dataset = concatenate_datasets([dataset_1, dataset_2]).select_columns(["query", "positive", "negative"])
 
 
 @dataclass
 class SwimIR:
-    dataset = load_dataset("nthakur/swim-ir-monolingual", "id", split="train")
+    dataset = load_dataset("nthakur/swim-ir-monolingual", "id", split="train", **kwargs)
 
-    @staticmethod
-    def train_samples() -> List[InputExample]:
+    def __post_init__(self):
+        columns = ["anchor", "positive", "negative"]
+        train_samples = self.train_samples()
+        self.dataset = Dataset.from_list([dict(zip(columns, sample)) for sample in train_samples])
+
+    def train_samples(self) -> Dataset:
         train_data = {}
         train_samples = []
 
-        for datum in SwimIR.dataset:
+        for datum in self.dataset:
             query = datum["query"].strip()
             answer = datum["text"].strip()
             title = datum["title"].strip()
@@ -226,76 +197,50 @@ class SwimIR:
                     # get random negative sample, from different query
                     random_query = random.choice([q for q in passage_queries if q != query])
                     negative = random.choice(queries[random_query])
-
-                    train_samples.append(InputExample(texts=[query, positive, negative]))
+                    train_samples.append([query, positive, negative])
 
         return train_samples
 
 
 @dataclass
 class IndoStoryCloze:
-    dataset = load_dataset("indolem/indo_story_cloze", split="train", trust_remote_code=True)
-
-    @staticmethod
-    def train_samples() -> List[InputExample]:
-        train_samples = []
-
-        for datum in IndoStoryCloze.dataset:
-            context = ". ".join([datum["sentence-1"], datum["sentence-2"], datum["sentence-3"], datum["sentence-4"]])
-            train_samples.append(
-                InputExample(
-                    texts=[
-                        context,
-                        datum["correct_ending"],
-                        datum["incorrect_ending"],
-                    ]
-                )
-            )
-
-        return train_samples
+    dataset = load_dataset("indolem/indo_story_cloze", split="train", **kwargs)
+    dataset = dataset.map(
+        lambda s1, s2, s3, s4: {"context": ". ".join([s1, s2, s3, s4])},
+        input_columns=["sentence-1", "sentence-2", "sentence-3", "sentence-4"],
+    )
+    dataset = dataset.select_columns(["context", "correct_ending", "incorrect_ending"])
 
 
 @dataclass
 class IndoNLI:
-    dataset = load_dataset("indonli", split="train", trust_remote_code=True)
-    id2label = {0: "entailment", 1: "neutral", 2: "contradiction"}
+    dataset = load_dataset("indonli", split="train", **kwargs)
 
-    @staticmethod
-    def train_samples() -> List[InputExample]:
+    def __post_init__(self):
+        columns = ["premise", "hypothesis", "contradiction"]
+        train_samples = self.train_samples()
+        self.dataset = Dataset.from_list([dict(zip(columns, sample)) for sample in train_samples])
+
+    def train_samples(self) -> Dataset:
+        id2label = {0: "entailment", 1: "neutral", 2: "contradiction"}
+        train_data = {}
+        train_samples = []
+
         def add_to_samples(sent1, sent2, label):
             if sent1 not in train_data:
                 train_data[sent1] = {"contradiction": set(), "entailment": set(), "neutral": set()}
             train_data[sent1][label].add(sent2)
 
-        train_data = {}
-        train_samples = []
-
-        for datum in IndoNLI.dataset:
+        for datum in self.dataset:
             sent1 = datum["premise"].strip()
             sent2 = datum["hypothesis"].strip()
-
-            add_to_samples(sent1, sent2, IndoNLI.id2label[datum["label"]])
-            add_to_samples(sent2, sent1, IndoNLI.id2label[datum["label"]])  # Also add the opposite
+            add_to_samples(sent1, sent2, id2label[datum["label"]])
+            add_to_samples(sent2, sent1, id2label[datum["label"]])  # Also add the opposite
 
         for sent1, others in train_data.items():
             if len(others["entailment"]) > 0 and len(others["contradiction"]) > 0:
-                train_samples.append(
-                    InputExample(
-                        texts=[
-                            sent1,
-                            random.choice(list(others["entailment"])),
-                            random.choice(list(others["contradiction"])),
-                        ]
-                    )
-                )
-                train_samples.append(
-                    InputExample(
-                        texts=[
-                            random.choice(list(others["entailment"])),
-                            sent1,
-                            random.choice(list(others["contradiction"])),
-                        ]
-                    )
-                )
+                entailments, contradictions = list(others["entailment"]), list(others["contradiction"])
+                train_samples.append([sent1, random.choice(entailments), random.choice(contradictions)])
+                train_samples.append([sent1, random.choice(entailments), random.choice(contradictions)])
 
         return train_samples
--- training/all/train_all_cached_mnrl.py
+++ training/all/train_all_cached_mnrl.py
@@ -1,27 +1,17 @@
 from dataclasses import dataclass
-import math
 
 from datargs import parse
 from datasets import load_dataset
-from sentence_transformers import SentenceTransformer, InputExample, models, losses
-from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
-
-from all_datasets import (
-    IndoNLI,
-    IndoStoryCloze,
-    mMARCO,
-    MIRACL,
-    SwimIR,
-    MultilingualNLI,
-    WReTE,
-    IndoLEMNTP,
-    TyDiQA,
-    FacQA,
-    LFQAID,
-    IndoQA,
-    ParaphraseDetection,
+from sentence_transformers import SentenceTransformer, models, losses
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, SimilarityFunction
+from sentence_transformers.trainer import SentenceTransformerTrainer
+from sentence_transformers.training_args import (
+    BatchSamplers,
+    MultiDatasetBatchSamplers,
+    SentenceTransformerTrainingArguments,
 )
-from MultiDatasetDataLoader import MultiDatasetDataLoader
+
+from all_datasets import *
 
 
 @dataclass
@@ -30,22 +20,21 @@ class Args:
     model_name: str = "indobenchmark/indobert-base-p1"
     # train
     max_seq_length: int = 128
-    # test
-    test_dataset_name: str = "LazarusNLP/stsb_mt_id"
-    test_dataset_split: str = "validation"
-    test_text_column_1: str = "text_1"
-    test_text_column_2: str = "text_2"
-    test_label_column: str = "correlation"
+    # eval
+    eval_dataset_name: str = "LazarusNLP/stsb_mt_id"
+    eval_dataset_split: str = "validation"
+    eval_text_column_1: str = "text_1"
+    eval_text_column_2: str = "text_2"
+    eval_label_column: str = "correlation"
     # training args
     num_epochs: int = 5
-    train_batch_size_pairs: int = 384
-    train_batch_size_triplets: int = 256
-    test_batch_size: int = 32
+    train_batch_size: int = 256
+    eval_batch_size: int = 32
     mini_batch_size: int = 128
     learning_rate: float = 2e-5
     warmup_ratio: float = 0.1
     output_path: str = "exp/all-indobert-base"
-    use_amp: bool = True
+    bf16: bool = True
     # huggingface hub args
     hub_model_id: str = "LazarusNLP/all-indobert-base"
     hub_private_repo: bool = True
@@ -59,18 +48,33 @@ def main(args: Args):
         "unicamp-dl/mmarco": mMARCO,
         "miracl/miracl": MIRACL,
         "nthakur/swim-ir-monolingual": SwimIR,
-        "LazarusNLP/multilingual-NLI-26lang-2mil7-id": MultilingualNLI,
-        "SEACrowd/wrete": WReTE,
-        "SEACrowd/indolem_ntp": IndoLEMNTP,
+        "LazarusNLP/multilingual-NLI-26lang-2mil7-id": MultilingualNLIEntailmentPairs,
+        "SEACrowd/wrete": WReTEEntailmentPairs,
+        "SEACrowd/indolem_ntp": IndoLEMNTPEntailmentPairs,
         "khalidalt/tydiqa-goldp": TyDiQA,
         "SEACrowd/facqa": FacQA,
         "indonesian-nlp/lfqa_id": LFQAID,
         "jakartaresearch/indoqa": IndoQA,
         "jakartaresearch/id-paraphrase-detection": ParaphraseDetection,
+        "wikimedia/wikipedia": Wikipedia,
+        "lesserfield/brainly": Brainly,
+        "esteler-ai/idn-news-az": IndonesianNews,
+        "hermanshid/doctor-id-qa": DoctorQA,
+        "SEACrowd/liputan6": Liputan6,
     }
 
-    train_ds = [ds.train_samples() for ds in raw_datasets.values()]
-    test_ds = load_dataset(args.test_dataset_name, split=args.test_dataset_split)
+    train_dataset = {name: ds().dataset for name, ds in raw_datasets.items()}
+
+    eval_dataset = load_dataset(args.eval_dataset_name, split=args.eval_dataset_split)
+    eval_dataset = eval_dataset.map(lambda score: {"score": float(score) / 5.0}, input_columns=[args.eval_label_column])
+    eval_dataset = eval_dataset.select_columns([args.eval_text_column_1, args.eval_text_column_2, "score"])
+    evaluator = EmbeddingSimilarityEvaluator(
+        sentences1=eval_dataset[args.eval_text_column_1],
+        sentences2=eval_dataset[args.eval_text_column_2],
+        scores=eval_dataset["score"],
+        main_similarity=SimilarityFunction.COSINE,
+        name="sts-dev",
+    )
 
     # Intialize model with mean pool
     word_embedding_model = models.Transformer(args.model_name, max_seq_length=args.max_seq_length)
@@ -78,48 +82,38 @@ def main(args: Args):
     pooling_model = models.Pooling(dimension, pooling_mode="mean")
     model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-    # DataLoader to batch your data
-    train_dataloader = MultiDatasetDataLoader(
-        train_ds, batch_size_pairs=args.train_batch_size_pairs, batch_size_triplets=args.train_batch_size_triplets
+    mnrl_loss = losses.CachedMultipleNegativesRankingLoss(model, mini_batch_size=args.mini_batch_size)
+
+    args = SentenceTransformerTrainingArguments(
+        output_dir=args.output_path,
+        num_train_epochs=args.num_epochs,
+        per_device_train_batch_size=args.train_batch_size,
+        per_device_eval_batch_size=args.eval_batch_size,
+        learning_rate=args.learning_rate,
+        warmup_ratio=args.warmup_ratio,
+        bf16=args.bf16,
+        batch_sampler=BatchSamplers.NO_DUPLICATES,
+        multi_dataset_batch_sampler=MultiDatasetBatchSamplers.PROPORTIONAL,
+        load_best_model_at_end=True,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        push_to_hub=True,
+        hub_model_id=args.hub_model_id,
+        hub_private_repo=args.hub_private_repo,
     )
 
-    warmup_steps = math.ceil(
-        len(train_dataloader) * args.num_epochs * args.warmup_ratio
-    )  # 10% of train data for warm-up
-
-    # Setup test data for evaluation
-    test_data = [
-        InputExample(
-            texts=[data[args.test_text_column_1], data[args.test_text_column_2]],
-            label=float(data[args.test_label_column]) / 5.0,
-        )
-        for data in test_ds
-    ]
-
-    evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_data, batch_size=args.test_batch_size)
-
-    # Use the denoising auto-encoder loss
-    train_loss = losses.CachedMultipleNegativesRankingLoss(model, mini_batch_size=args.mini_batch_size)
-
-    # Call the fit method
-    model.fit(
-        train_objectives=[(train_dataloader, train_loss)],
+    trainer = SentenceTransformerTrainer(
+        model=model,
+        args=args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        loss=mnrl_loss,
         evaluator=evaluator,
-        epochs=args.num_epochs,
-        warmup_steps=warmup_steps,
-        show_progress_bar=True,
-        optimizer_params={"lr": args.learning_rate, "eps": 1e-6},
-        output_path=args.output_path,
-        save_best_model=True,
-        use_amp=args.use_amp,
     )
 
-    # Save model to HuggingFace Hub
-    model.save_to_hub(
-        args.hub_model_id,
-        private=args.hub_private_repo,
-        train_datasets=list(raw_datasets.keys()),
-    )
+    trainer.train()
+
+    trainer.push_to_hub(dataset=list(raw_datasets.keys()))
 
 
 if __name__ == "__main__":
